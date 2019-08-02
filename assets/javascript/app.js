909818dfd169bab051d201dad740582e22cce0d2
Peter Geyser <peterpgeyser@gmail.com>
2019-08-02 12:50:33 -0700

    createButton function complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,68 @@
 $(document).ready(function () {
 
     //topics variable that buttons function will populate from
-        var topics = ["sad", "happy", "angry", "thanks", "excited"];
+    var topics = ["sad", "happy", "angry", "thanks", "excited"];
 
-    //function to create buttons based on topics var
-        //for loop that iterates through topics var
-            //var that creates a new <button> html tag
-            //necessary classes: btn, btn-primary, button-reaction
-            //necessary attr: data-reaction + value
-            //append to #reaction-buttons div
+    //run createButton function
+    createButton();
 
     //button-submit on click function
-        //stop page from refreshing
-        //grab value from user input text field
-        //push value into topics array
-        //clear text input field
-        //run createButton function
+    //stop page from refreshing
+    //grab value from user input text field
+    //push value into topics array
+    //clear text input field
+    //run createButton function
 
     //button-reaction on click function
-        //Grab data-reaction value from button
-        //Create queryURL
-        //Run AJAX request with queryURL
-        //.then function
-            //store response in a var
-            //For loop through each result item
-                //create new div
-                //create new p tag for result's rating
-                //create new img div
-                    //create and store image tag for still image
-                        //set src to still image
-                    //creat and store image tag for animated image
-                //necessary attr: data-state
-                    //set data-state to still
-                //necessary class: .gif
-    
-    //.gif on click function
-        //grab data-state value and store in var
-        //if state === still
-            //change src to animated gif url
-            //change data-state to animate
+    //Grab data-reaction value from button
+    //Create queryURL
+    //Run AJAX request with queryURL
+    //.then function
+    //store response in a var
+    //For loop through each result item
+    //create new div
+    //create new p tag for result's rating
+    //create new img div
+    //create and store image tag for still image
+    //set src to still image
+    //creat and store image tag for animated image
+    //necessary attr: data-state
+    //set data-state to still
+    //necessary class: .gif
 
-        //else
-            //change src to still gif url
-            //change data-state to still
+    //.gif on click function
+    //grab data-state value and store in var
+    //if state === still
+    //change src to animated gif url
+    //change data-state to animate
+
+    //else
+    //change src to still gif url
+    //change data-state to still
+
+    //function to create buttons based on topics var
+    function createButton() {
+
+        //for loop that iterates through topics var
+        for (var i = 0; i < topics.length; i++) {
+
+            //var that creates a new <button> html tag
+            var button = $('<button>');
+
+            //necessary classes: btn, btn-primary, button-reaction
+            button.addClass("btn").addClass("btn-primary").addClass("button-reaction");
+
+            //necessary attr: data-reaction + value
+            button.attr("data-reaction", topics[i]);
+
+            //add buttont text
+            button.text(topics[i]);
+
+            //append to #reaction-buttons div
+            $("#reaction-buttons").append(button);
+
+        }
+
+    }
 
 })
