78ea4845e5c97404ab7516bb6196d2cde878966d
Peter Geyser <peterpgeyser@gmail.com>
2019-08-02 10:31:44 -0700

    started pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    //topics variable that buttons function will populate from
+        var topics = ["sad", "happy", "angry", "thanks", "excited"];
+
+    //function to create buttons based on topics var
+        //for loop that iterates through topics var
+            //var that creates a new html tag <button> 
+            //necessary classes: btn, btn-primary, button-reaction
+            //necessary attr: data-reaction + value
+            //append to #reaction-buttons div
+
+    //button-submit on click function
+        //
+
+    //button-reaction on click function
+
+})
